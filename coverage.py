b37a860c5a503f6d99d8c8d5a44a8ca151ff0192
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-05-11 10:15:48 +0200

    fixed coverage file

--- coverage.py
+++ coverage.py
@@ -6,22 +6,81 @@ rustup component add --toolchain nightly llvm-tools-preview
 """
 import os
 import re
+import sys
 import subprocess
+from glob import glob
 
 # Compute the coverage for all fuzzing targets, or not
 FUZZING = False
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 target_folder = os.path.join(ROOT, "target")
-test_cov_path = os.path.join(target_folder, "test_cov.profraw")
+# Get info about the rust installation
 rustup_info = subprocess.check_output("rustup show", shell=True).decode()
 arch = re.findall(r"Default host: (.+)", rustup_info)[0]
 
-# Get where it's installed
+# Get where LLVM is installed
 sysroot = subprocess.check_output("rustc --print sysroot", shell=True).decode().strip()
 llvm_path = os.path.join(sysroot, "lib", "rustlib", arch, "bin")
+
+# Check that people can read the doc
+if not os.path.exists(os.path.join(llvm_path, "llvm-profdata")):
+    print("PLEASE run:")
+    print("rustup component add --toolchain nightly llvm-tools-preview")
+    print("cargo install rustfilt")
+    sys.exit(1)
+
+# Settings
+test_cov_path = os.path.join(target_folder, "cargo-test-cov-%p-%m.profraw")
 final_cov_path = os.path.join(target_folder, "total_coverage.profdata")
-test_cov_path = os.path.join(target_folder, "test_cov.profdata")
+doc_tests_bin = os.path.join(target_folder, "doctestbins")
+
+cov_files = []
+exec_files = []
+
+# Clean up the targets folder
+subprocess.check_call(
+    "cargo clean",
+    shell=True,
+    cwd=ROOT,
+)
+# Create a folder for the test coverage
+os.makedirs(target_folder, exist_ok=True)
+
+out = subprocess.check_output(
+    "cargo test",
+    shell=True,
+    cwd=ROOT,
+    stderr=subprocess.STDOUT,
+    universal_newlines=True,
+    env={
+        **os.environ,
+        "CARGO_INCREMENTAL":"0",
+        "RUSTFLAGS": "-C instrument-coverage",
+        "RUSTDOCFLAGS":"-Zinstrument-coverage -Z unstable-options --persist-doctests {}".format(doc_tests_bin),
+        "LLVM_PROFILE_FILE": test_cov_path,
+    },
+)
+
+# For the doctests see:
+# https://github.com/rust-lang/rust/issues/79417
+
+# add the test binaries
+exec_files.extend(
+    os.path.join(ROOT, file)
+    for file in re.findall(r"Running \S+ \((target/.+?)\)", out)
+)
+# add all the doc tests binaries
+exec_files.extend(
+    file
+    for file in glob(os.path.join(doc_tests_bin, "**", "*"))
+)
+cov_files.extend(
+    os.path.join(os.path.dirname(test_cov_path), file)
+    for file in os.listdir(os.path.dirname(test_cov_path))
+    if file.endswith(".profraw")
+)
+
 if FUZZING:
     # Get the list of fuzzing targets
     fuzz_targets = (
@@ -33,27 +92,6 @@ if FUZZING:
         .decode()
         .split("\n")[:-1]
     )
-# Clean up the targets folder
-subprocess.check_call(
-    "cargo clean",
-    shell=True,
-    cwd=ROOT,
-)
-# Create a folder for the test coverage
-os.makedirs(target_folder, exist_ok=True)
-# Generate coverage from the test
-subprocess.check_call(
-    "cargo test",
-    shell=True,
-    cwd=ROOT,
-    env={
-        **os.environ,
-        "RUSTFLAGS": "-C instrument-coverage",
-        "LLVM_PROFILE_FILE": test_cov_path,
-    },
-)
-
-if FUZZING:
     # Generate coverage for all the targets
     for fuzz_target in fuzz_targets:
         subprocess.check_call(
@@ -61,55 +99,37 @@ if FUZZING:
             shell=True,
             cwd=ROOT,
         )
-    fuzz_paths = [
+
+    cov_files.extend(
         os.path.join(ROOT, "fuzz", "coverage", fuzz_target, "coverage.profdata")
         for fuzz_target in fuzz_targets
-    ]
-else:
-    fuzz_paths = []
-
+    )
+    exec_files.extend(
+        os.path.join(
+            ROOT, "target", arch, "coverage", arch, "release", fuzz_target
+        )
+        for fuzz_target in fuzz_targets
+    )
 
 # Merge the coverages into an unique file
 subprocess.check_call(
     "{}/llvm-profdata merge -sparse {} -o {}".format(
         llvm_path,
-        " ".join(
-            [test_cov_path] + fuzz_paths
-        ),
+        " ".join(cov_files),
         final_cov_path,
     ),
     shell=True,
     cwd=ROOT,
 )
 
-if FUZZING:
-    fuzz_paths = [  # TODO!: add also doc tests binary
-        os.path.join(
-            ROOT, "target", arch, "coverage", arch, "release", fuzz_target
-        )
-        for fuzz_target in fuzz_targets
-    ]
-else:
-    fuzz_paths = []
-
-test_execs = [
-    path
-    for path in [
-        os.path.join(ROOT, "target", "debug", "deps", file)
-        for file in os.listdir(os.path.join(ROOT, "target", "debug", "deps"))
-    ]
-    if os.access(path, os.X_OK) and os.path.basename(path).startswith("test_")
-]
-
 # Create the report!
 subprocess.check_call(
     (
-        "{}/llvm-cov report --Xdemangler=rustfilt --instr-profile={} {} "
-        "-ignore-filename-regex='.cargo' "
+        "{}/llvm-cov report --Xdemangler=rustfilt --instr-profile={} -object {} "
     ).format(
         llvm_path,
         final_cov_path,
-        " ".join(test_execs + fuzz_paths),
+        " -object ".join(exec_files),
     ),
     shell=True,
     cwd=ROOT,
