fa2f6499e891faf3ca8b98616a2aa8e85590a55b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2023-05-15 00:01:42 +0200

    Added method to merge an iterator of many HLL counters

--- src/iter.rs
+++ src/iter.rs
@@ -2,12 +2,25 @@
 //! using a HyperLogLog data structure.
 //!
 //! # Example
-//!
+//! You can estimate the cardinality of an iterator using the `estimate_cardinality` method.
 //! ```
 //! use hyperloglog_rs::prelude::*;
 //!
 //! let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //! let cardinality_estimate = v.iter().estimate_cardinality::<12, 5>();
+//! assert!((cardinality_estimate - 10.0).abs() < 1.0);
+//! ```
+//!
+//! You can merge multiple HyperLogLog counters from iterators using the `union` method.
+//!
+//! ```
+//! use hyperloglog_rs::prelude::*;
+//!
+//! let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//! let hll: HyperLogLog<12, 6> = v.into_iter().map(|index|{
+//!     HyperLogLog::from(index)
+//! }).union();
+//! let cardinality_estimate = hll.estimate_cardinality();
 //! assert!((cardinality_estimate - 10.0).abs() < 1.0);
 //! ```
 use core::hash::Hash;
@@ -42,5 +55,49 @@ where
     {
         let hll: HyperLogLog<PRECISION, BITS> = self.collect();
         hll.estimate_cardinality()
+    }
+}
+
+pub trait HyperLogLogIterator<const PRECISION: usize, const BITS: usize> {
+    /// Returns a HyperLogLog that is the union of all HyperLogLogs in the iterator.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use hyperloglog_rs::prelude::*;
+    ///
+    /// let mut hll1 = HyperLogLog::<12, 6>::new();
+    /// hll1.insert(&1);
+    /// hll1.insert(&2);
+    ///
+    /// let mut hll2 = HyperLogLog::<12, 6>::new();
+    /// hll2.insert(&3);
+    /// hll2.insert(&4);
+    ///
+    /// let mut hll3 = HyperLogLog::<12, 6>::new();
+    /// hll3.insert(&5);
+    /// hll3.insert(&6);
+    ///
+    /// let hll_union = vec![hll1, hll2, hll3].into_iter().union();
+    ///
+    /// assert!(hll_union.estimate_cardinality() - 6.0 < 1.0, "Expected 6.0, got {}", hll_union.estimate_cardinality());
+    /// ```
+    fn union(self) -> HyperLogLog<PRECISION, BITS>
+    where
+        [(); ceil(1 << PRECISION, 32 / BITS)]:;
+}
+
+impl<const PRECISION: usize, const BITS: usize, I> HyperLogLogIterator<PRECISION, BITS> for I
+where
+    I: Iterator<Item = HyperLogLog<PRECISION, BITS>>,
+    [(); ceil(1 << PRECISION, 32 / BITS)]:,
+{
+    fn union(self) -> HyperLogLog<PRECISION, BITS>
+    where
+        [(); ceil(1 << PRECISION, 32 / BITS)]:,
+    {
+        self.fold(HyperLogLog::<PRECISION, BITS>::default(), |acc, hll| {
+            acc | hll
+        })
     }
 }
