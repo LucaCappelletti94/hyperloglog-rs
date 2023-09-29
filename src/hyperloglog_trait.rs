cde339663a4bc67537c136b62b354961b383481b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2023-09-29 11:17:42 +0200

    Updated benchmark for union cardinality to mine data for parameters tuning

--- src/hyperloglog_trait.rs
+++ src/hyperloglog_trait.rs
@@ -292,64 +292,6 @@ pub trait HyperLogLogTrait<
     }
 
     #[inline(always)]
-    /// Returns an estimate of the cardinality of the two HLL counters union.
-    fn estimate_union_and_sets_cardinality_old<F: Primitive<f32> + MaxMin>(
-        &self,
-        other: &Self,
-    ) -> EstimatedUnionCardinalities<F> {
-        let mut raw_union_estimate = 0.0;
-        let mut raw_left_estimate = 0.0;
-        let mut raw_right_estimate = 0.0;
-
-        let mut union_zeros = 0;
-        for (left_word, right_word) in self
-            .get_words()
-            .iter_elements()
-            .copied()
-            .zip(other.get_words().iter_elements().copied())
-        {
-            let mut union_partial: f32 = 0.0;
-            let mut left_partial: f32 = 0.0;
-            let mut right_partial: f32 = 0.0;
-            for i in 0..Self::NUMBER_OF_REGISTERS_IN_WORD {
-                let left_register = (left_word >> (i * BITS)) & Self::LOWER_REGISTER_MASK;
-                let right_register = (right_word >> (i * BITS)) & Self::LOWER_REGISTER_MASK;
-                let maximal_register = (left_register).max(right_register);
-                union_partial += f32::from_le_bytes(((127 - maximal_register) << 23).to_le_bytes());
-                left_partial += f32::from_le_bytes(((127 - left_register) << 23).to_le_bytes());
-                right_partial += f32::from_le_bytes(((127 - right_register) << 23).to_le_bytes());
-                union_zeros += (maximal_register == 0) as usize;
-            }
-            raw_union_estimate += union_partial;
-            raw_left_estimate += left_partial;
-            raw_right_estimate += right_partial;
-        }
-
-        union_zeros -= Self::get_number_of_padding_registers();
-
-        // We need to subtract the padding registers from the raw estimates
-        // as for each such register we are adding a one.
-        raw_union_estimate -= Self::get_number_of_padding_registers() as f32;
-        raw_left_estimate -= Self::get_number_of_padding_registers() as f32;
-        raw_right_estimate -= Self::get_number_of_padding_registers() as f32;
-
-        let union_estimate = F::reverse(Self::adjust_estimate_with_zeros(
-            raw_union_estimate,
-            union_zeros,
-        ));
-        let left_estimate = F::reverse(Self::adjust_estimate_with_zeros(
-            raw_left_estimate,
-            self.get_number_of_zero_registers(),
-        ));
-        let right_estimate = F::reverse(Self::adjust_estimate_with_zeros(
-            raw_right_estimate,
-            other.get_number_of_zero_registers(),
-        ));
-
-        EstimatedUnionCardinalities::from((left_estimate, right_estimate, union_estimate))
-    }
-
-    #[inline(always)]
     /// Returns an estimate of the cardinality of the intersection of two HyperLogLog counters.
     ///
     /// This method calculates an estimate of the cardinality of the intersection of two HyperLogLog
--- tests/test_union_cardinality.rs
+++ tests/test_union_cardinality.rs
@@ -16,9 +16,10 @@
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::Write;
-
+use rayon::prelude::*;
 use hyperloglog_rs::prelude::*;
-use indicatif::ProgressIterator;
+use indicatif::ParallelProgressIterator;
+use rayon::prelude::IntoParallelIterator;
 
 fn splitmix64(mut x: u64) -> u64 {
     x = x.wrapping_add(0x9E3779B97F4A7C15);
@@ -38,47 +39,25 @@ fn union(set1: &HashSet<u64>, set2: &HashSet<u64>) -> usize {
     set1.union(set2).count()
 }
 
-fn recent_union_hll<PRECISION: Precision + WordType<BITS>, const BITS: usize>(
-    vec1: &Vec<u64>,
-    vec2: &Vec<u64>,
-) -> f32 {
-    let hll1: HyperLogLog<PRECISION, BITS> = vec1.iter().collect();
-    let hll2: HyperLogLog<PRECISION, BITS> = vec2.iter().collect();
-
-    hll1.estimate_union_cardinality(&hll2)
-}
-
-fn original_union_hll<PRECISION: Precision + WordType<BITS>, const BITS: usize>(
-    vec1: &Vec<u64>,
-    vec2: &Vec<u64>,
-) -> f32 {
-    let hll1: HyperLogLog<PRECISION, BITS> = vec1.iter().collect();
-    let hll2: HyperLogLog<PRECISION, BITS> = vec2.iter().collect();
-
-    hll1.estimate_union_and_sets_cardinality_old(&hll2)
-        .get_union_cardinality()
-}
-
 fn write_line<PRECISION: Precision + WordType<BITS>, const BITS: usize>(
     vec1: &Vec<u64>,
     vec2: &Vec<u64>,
-    set1_str: &str,
-    set2_str: &str,
     exact_union: usize,
     file: &mut File,
 ) -> std::io::Result<()> {
-    let old_hll = original_union_hll::<PRECISION, BITS>(&vec1, &vec2);
-    let recent_hll = recent_union_hll::<PRECISION, BITS>(&vec1, &vec2);
+    let hll1: HyperLogLog<PRECISION, BITS> = vec1.iter().collect();
+    let hll2: HyperLogLog<PRECISION, BITS> = vec2.iter().collect();
+
+    let approximation = hll1.estimate_union_cardinality(&hll2);
 
     let line = format!(
-        "{}\t{}\t{}\t{}\t{}\n",
+        "{}\t{}\t{}\t{}\t{}\t{}\n",
         PRECISION::EXPONENT,
         BITS,
         exact_union,
-        old_hll,
-        recent_hll,
-        // set1_str,
-        // set2_str
+        approximation,
+        hll1.get_number_of_zero_registers(),
+        hll2.get_number_of_zero_registers(),
     );
 
     file.write_all(line.as_bytes())
@@ -89,49 +68,41 @@ fn write_line_set<
 >(
     vec1: &Vec<u64>,
     vec2: &Vec<u64>,
-    set1_str: &str,
-    set2_str: &str,
     exact_union: usize,
     file: &mut File,
 ) {
-    write_line::<PRECISION, 1>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file).unwrap();
-    write_line::<PRECISION, 2>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file).unwrap();
-    write_line::<PRECISION, 3>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file).unwrap();
-    write_line::<PRECISION, 4>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file).unwrap();
-    write_line::<PRECISION, 5>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file).unwrap();
-    write_line::<PRECISION, 6>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file).unwrap();
+    write_line::<PRECISION, 1>(&vec1, &vec2,  exact_union, file).unwrap();
+    write_line::<PRECISION, 2>(&vec1, &vec2,  exact_union, file).unwrap();
+    write_line::<PRECISION, 3>(&vec1, &vec2,  exact_union, file).unwrap();
+    write_line::<PRECISION, 4>(&vec1, &vec2,  exact_union, file).unwrap();
+    write_line::<PRECISION, 5>(&vec1, &vec2,  exact_union, file).unwrap();
+    write_line::<PRECISION, 6>(&vec1, &vec2,  exact_union, file).unwrap();
 }
 
 fn write_line_set_for_hasher(
     vec1: &Vec<u64>,
     vec2: &Vec<u64>,
-    set1_str: &str,
-    set2_str: &str,
     exact_union: usize,
     file: &mut File,
 ) {
-    write_line_set::<Precision4>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
-    write_line_set::<Precision5>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
-    write_line_set::<Precision6>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
-    write_line_set::<Precision7>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
-    write_line_set::<Precision8>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
-    write_line_set::<Precision9>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
-    write_line_set::<Precision10>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
-    write_line_set::<Precision11>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
-    write_line_set::<Precision12>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
-    write_line_set::<Precision13>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
-    write_line_set::<Precision14>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
-    write_line_set::<Precision15>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
-    write_line_set::<Precision16>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
-    write_line_set::<Precision17>(&vec1, &vec2, &set1_str, &set2_str, exact_union, file);
+    write_line_set::<Precision4>(&vec1, &vec2, exact_union, file);
+    write_line_set::<Precision5>(&vec1, &vec2, exact_union, file);
+    write_line_set::<Precision6>(&vec1, &vec2, exact_union, file);
+    write_line_set::<Precision7>(&vec1, &vec2, exact_union, file);
+    write_line_set::<Precision8>(&vec1, &vec2, exact_union, file);
+    write_line_set::<Precision9>(&vec1, &vec2, exact_union, file);
+    write_line_set::<Precision10>(&vec1, &vec2, exact_union, file);
+    write_line_set::<Precision11>(&vec1, &vec2, exact_union, file);
+    write_line_set::<Precision12>(&vec1, &vec2, exact_union, file);
+    write_line_set::<Precision13>(&vec1, &vec2, exact_union, file);
+    write_line_set::<Precision14>(&vec1, &vec2, exact_union, file);
+    write_line_set::<Precision15>(&vec1, &vec2, exact_union, file);
+    write_line_set::<Precision16>(&vec1, &vec2, exact_union, file);
+    write_line_set::<Precision17>(&vec1, &vec2, exact_union, file);
 }
 
-//#[test]
+#[test]
 fn test_union_cardinality_perfs() {
-    let mut file = File::create("union_cardinality_benchmark.tsv").unwrap();
-    file.write_all(b"precision\tbits\texact\told\trecent\thash_name\n")
-        .unwrap();
-
     // since both the precision and the number of bits are compile time constants, we can
     // not iterate over the precision and bits, but we need to manually change them, making
     // the code a bit verbose:
@@ -141,20 +112,31 @@ fn test_union_cardinality_perfs() {
 
     // For each precision and number of bits, we generate 1000 random sets and write them to the file.
     // We also write the exact union similarity and the estimated union similarity using HyperLogLog.
-    for i in (0..2_000_u64).progress() {
+    (0..100_000_000_u64).into_par_iter().progress().for_each(|i|{
+        let path = format!("union_cardinality_benchmark_{}.tsv", i);
+
+        // If the path already exists, we skip it.
+        if std::path::Path::new(&path).exists() {
+            return;
+        }
+
+        let mut file = File::create(path).unwrap();
+        file.write_all(b"precision\tbits\texact\tapproximation\tnumber_of_zeros_left\tnumber_of_zeros_right\n")
+            .unwrap();
+        
         let seed = (i + 1).wrapping_mul(234567898765);
         let mut rng = splitmix64(seed);
 
         let mut set1 = HashSet::new();
         let mut set2 = HashSet::new();
 
-        let first_set_cardinality = xorshift(rng) % 100_000;
+        let first_set_cardinality = xorshift(rng) % 100_000_000;
         rng = splitmix64(rng);
-        let second_set_cardinality = xorshift(rng) % 100_000;
+        let second_set_cardinality = xorshift(rng) % 100_000_000;
         rng = splitmix64(rng);
-        let first_world_size = xorshift(rng) % 100_000;
+        let first_world_size = xorshift(rng) % 100_000_000;
         rng = splitmix64(rng);
-        let second_world_size = xorshift(rng) % 100_000;
+        let second_world_size = xorshift(rng) % 100_000_000;
         rng = splitmix64(rng);
 
         let mut vec1 = Vec::new();
@@ -176,28 +158,8 @@ fn test_union_cardinality_perfs() {
 
         let exact = union(&set1, &set2);
 
-        let set1_str = set1
-            .iter()
-            .map(|x| x.to_string())
-            .collect::<Vec<String>>()
-            .join(",");
-        let set2_str = set2
-            .iter()
-            .map(|x| x.to_string())
-            .collect::<Vec<String>>()
-            .join(",");
-
         write_line_set_for_hasher(
-            &vec1, &vec2, &set1_str, &set2_str, exact, &mut file,
+            &vec1, &vec2, exact, &mut file,
         );
-        // write_line_set_for_hasher::<SipHasher24>(
-        //     &vec1, &vec2, &set1_str, &set2_str, exact, &mut file,
-        // );
-        // write_line_set_for_hasher::<MetroHasher>(
-        //     &vec1, &vec2, &set1_str, &set2_str, exact, &mut file,
-        // );
-        // write_line_set_for_hasher::<HighwayHasher>(
-        //     &vec1, &vec2, &set1_str, &set2_str, exact, &mut file,
-        // );
-    }
+    });
 }
