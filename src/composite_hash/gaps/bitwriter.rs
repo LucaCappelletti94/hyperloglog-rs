ab5746b8de4e1dce502010771ef95f7c00541353
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2024-08-27 18:02:36 +0200

    solved seek + flush of writer

--- src/composite_hash/gaps/bitwriter.rs
+++ src/composite_hash/gaps/bitwriter.rs
@@ -31,8 +31,8 @@ impl<'a> BitWriter<'a> {
             let buffer = self.buffer << self.space_left_in_buffer;
             let old_word = u64::from_be(self.data[self.word_idx]);
             let clean_old_word = (old_word << to_flush) >> to_flush;
-            let new_word = (clean_old_word | buffer).to_be();
-            self.data[self.word_idx] = new_word;
+            let new_word = clean_old_word | buffer;
+            self.data[self.word_idx] = new_word.to_be();
         }
     }
 
@@ -41,7 +41,8 @@ impl<'a> BitWriter<'a> {
         self.word_idx = bits_idx / 64;
         let idx_in_word = bits_idx % 64;
         self.space_left_in_buffer = 64 - idx_in_word;
-        self.buffer = u64::from_be(self.data[self.word_idx]);
+        let word = u64::from_be(self.data[self.word_idx]);
+        self.buffer = word >> (64 - idx_in_word);
     }
 
     pub fn tell(&self) -> usize {
@@ -203,28 +204,24 @@ mod testing_writer {
 
     #[test]
     fn test_hand_picked() {
-        let mut buffer: [u64; 2] = [
-            0b01110111_11000010_11110100_00100011_11101100_10100100_11001010_01110110_u64.to_be(),
-            0b11011101_00110110_10011010_00111110_10100000_11110101_01100101_01001010_u64.to_be(),
+        let mut buffer: [u64; 1] = [
+            0b01110111_11000010_11110100_00100011_11101100_10100100_11001010_10000001_u64.to_be(),
         ];
 
         let mut writer = BitWriter::new(&mut buffer);
         writer.seek(13);
-        writer.write_gamma(100); // 100 = 64 + 32 + 4 = 0000001100100
+        writer.write_gamma(100); // 100 -> 101 = 64 + 32 + 4 + 1= 0000001100101
         drop(writer);
 
-        let expected: [u64; 2] = [
-            //  0b01110111_11000010_11110100_00100011_11101100_10100100_11001010_01110110,
-            //                xxxx_xxxxxxxx_x
-            0b01110111_11000000_00110010_00100011_11101100_10100100_11001010_01110110_u64.to_be(),
-            0b11011101_00110110_10011010_00111110_10100000_11110101_01100101_01001010_u64.to_be(),
+        let expected: [u64; 1] = [
+            0b01110111_11000_0000001100101_100011_11101100_10100100_11001010_10000001_u64.to_be(),
         ];
-
+        
         assert_eq!(
             buffer,
             expected,
-            "Buffer is not as expected. Buffer[0] Xor Expected: {:064b}",
-            (buffer[0] ^ expected[0]).to_be()
+            "Buffer is not as expected. Xor: {:064b}",
+            (buffer[0] ^ expected[0]).to_be(),
         );
     }
 }
